9b7a71b7ba606258701ff5c5679ffb889f2f3721
John Doe <johndoe@example.com>
2016-06-26 16:24:15 -0400

    Styling on twitter politician-issue show page

--- db/migrate/20160625182715_create_interests.rb
+++ db/migrate/20160625182715_create_interests.rb
@@ -1,8 +1,8 @@
 class CreateInterests < ActiveRecord::Migration
   def change
     create_table :interests do |t|
-      t.references :politician
-      t.references :issue
+      t.references :politician, null: false
+      t.references :issue, null: false
 
       t.timestamps null: false
     end
