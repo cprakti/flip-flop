74220a67207aebb6ea3578b9debaa238ee1001a7
DianaEromosele <diana.ozemebhoya@gmail.com>
2016-06-27 03:38:08 -0400

    Tweak politician show page

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -10,5 +10,18 @@ class PoliticiansController < ApplicationController
             redirect_to root_path
         end
     end
+
+     def tweet_data
+    # point1 = model_method(1)
+    # point2 = model_method(2)
+        respond_to do |format|
+          format.json {
+            render :json => [55,66,77]
+          }
+        end
+    end
+
+   
+
 end
 
