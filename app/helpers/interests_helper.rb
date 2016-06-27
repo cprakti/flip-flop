0a49472450aa80f3717ed6374205f9f769b21508
ICalvinG <calvinieong1118@gmail.com>
2016-06-27 11:13:15 -0400

    Add clickable links, hashtags, and tags that direct to a new tab.

--- app/helpers/interests_helper.rb
+++ app/helpers/interests_helper.rb
@@ -5,19 +5,20 @@ module InterestsHelper
   		urls = new_string.scan(/(?:http|https):\/\/[a-z0-9]+(?:[\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(?:(?::[0-9]{1,5})?\/[^\s]*)?/)
   		hashtags = new_string.scan(/\B#\w*[a-zA-Z]+\w*/)
   		atsigns = new_string.scan(/\B@\w*[a-zA-Z]+\w*/)
-  		if atsigns.length > 0
-  			formatted_atsign = String.new(atsigns[0])
-  			formatted_atsign.tr!('@', '')
-  			new_string[formatted_atsign] = "<a href='https://twitter.com/#{formatted_atsign}'>#{formatted_atsign}</a>"
-  		elsif hashtags.length > 0
-  			formatted_hashtag = String.new(hashtags[0])
+        atsigns.each do |atsign|
+  			 formatted_atsign = String.new(atsign)
+  			 formatted_atsign.tr!('@', '')
+  			 new_string[formatted_atsign] = "<a target='blank' href='https://twitter.com/#{formatted_atsign}'>#{formatted_atsign}</a>"
+        end
+        hashtags.each do |hashtag|
+  			formatted_hashtag = String.new(hashtag)
   			formatted_hashtag.tr!('#', '')
-  			new_string[formatted_hashtag] = "<a href='https://twitter.com/#{formatted_hashtag}'>#{formatted_hashtag}</a>"
-  		elsif urls.length > 0
-  			formatted_url = String.new(urls[0])
-  			new_string[formatted_url] = "<a href='#{formatted_url}'>#{formatted_url}</a>"
-  		end
+  			new_string[formatted_hashtag] = "<a target='blank' href='https://twitter.com/#{formatted_hashtag}'>#{formatted_hashtag}</a>"
+        end
+        urls.each do |url|
+  			formatted_url = String.new(url)
+  			new_string[formatted_url] = "<a target='blank' href='#{formatted_url}'>#{formatted_url}</a>"
+        end
   		new_string
 	end
-
 end
