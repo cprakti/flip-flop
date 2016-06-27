40942afddd0086449003d720cec6b53d4c60caf0
John Doe <johndoe@example.com>
2016-06-27 14:29:32 -0400

    Add styling to homepage, twitter issues, layout pages

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -2,6 +2,11 @@ class IssuesController < ApplicationController
 
   def index
     @issues = Issue.order("name asc")
+    @gunlaws = Issue.find(1)
+    @immigration = Issue.find(2)
+    @lgbtq = Issue.find(3)
+    @economy = Issue.find(4)
+    @healthcare = Issue.find(5)
   end
 
   def show
