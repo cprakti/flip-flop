f279d719a766249f41aec86ed75c65eb74e022aa
DianaEromosele <diana.ozemebhoya@gmail.com>
2016-06-27 03:40:41 -0400

    Tweak poli controller

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -11,15 +11,15 @@ class PoliticiansController < ApplicationController
         end
     end
 
-     def tweet_data
-    # point1 = model_method(1)
-    # point2 = model_method(2)
-        respond_to do |format|
-          format.json {
-            render :json => [55,66,77]
-          }
-        end
-    end
+    #  def tweet_data
+    # # point1 = model_method(1)
+    # # point2 = model_method(2)
+    #     respond_to do |format|
+    #       format.json {
+    #         render :json => [55,66,77]
+    #       }
+    #     end
+    # end
 
    
 
