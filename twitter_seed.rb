e8816114c086aec7f42d7eef831bc7c44fb95efa
John Doe <chandnirankari@gmail.com>
2016-06-29 11:50:09 -0400

    Remove twitter seed file from git ignore file

---
+++ twitter_seed.rb
@@ -0,0 +1,72 @@
+Politician.create(name: "Barack Obama", political_party: "Democrat", title: "Senator", twitter_handle: "BarackObama", twitter_profile: "This account is run by Organizing for Action staff. Tweets from the President are signed -bo." image: "barack_obama.png")
+
+Politician.create(name: "Robert Aderholt", political_party: "Republican", title: "U.S. Representative, Alabama", twitter_handle: "Robert_Aderholt", twitter_profile: "Proudly serving the 4th Congressional District of Alabama in Congress" image: "robert_aderholt.png")
+
+Politician.create(name: "Mark Amodei", political_party: "Republican", title: "U.S. Representative, Nevada", twitter_handle: "MarkAmodeiNV2", twitter_profile: "Proudly representing Nevada's 2nd District" image: "mark_amodei.png")
+
+Politician.create(name: "Alma S. Adams", political_party: "Democrat", title: "U.S. Representative, North Carolina", twitter_handle: "RepAdams", twitter_profile: "Congresswoman. Educator. Artist." image: "alma_adams.png")
+
+Politician.create(name: "Pete Aguilar", political_party: "Democrat", title: "U.S. Representative, California", twitter_handle: "aguilarpete", twitter_profile: "Dad. Husband. Former pothole filling Mayor and newly elected Representative of California's 31st district. Living the dream. Personal account" image: "pete_aguilar.png")
+
+Politician.create(name: "Rick W. Allen", political_party: "Republican", title: "U.S. Representative, Georgia", twitter_handle: "aguilarpete", twitter_profile: "Proudly representing Georgia's 12th Congressional District" image: "rick_allen.png")
+
+Politician.create(name: "Brad Ashford", political_party: "Republican", title: "U.S. Representative, Georgia", twitter_handle: "RepBradAshford", twitter_profile: "Proudly representing Georgia's 12th Congressional District" image: "brad_ashford.png")
+
+Politician.create(name: "Ralph Abraham", political_party: "Republican", title: "U.S. Representative, Louisiana", twitter_handle: "RepAbraham", twitter_profile: "Proudly representing Louisiana's 5th District. Rural family physician, veteran, farmer, pilot, former veterinarian, husband, father & grandfather." image: "ralph_abraham")
+
+Politician.create(name: "Joe Barton", political_party: "Republican", title: "U.S. Representative, Texas", twitter_handle: "RepJoeBarton", twitter_profile: "I represent the 6th Congressional District of Texas." image: "joe_barton.png")
+
+Politician.create(name: "Xavier Becerra", political_party: "Democrat", title: "U.S. Representative, California", twitter_handle: "RepBecerra", twitter_profile: "Proudly representing California's 34th Congressional District." image: "xavier_becerra.png")
+
+Politician.create(name: "Sanford Bishop, Jr.", political_party: "Democrat", title: "U.S. Representative, Georgia", twitter_handle: "SanfordBishop", twitter_profile: "Proudly representing Georgia's 2nd Congressional District. Ranking Member of MilCon/VA Approps Subcommittee." image: "sanford_bishop.png")
+
+Politician.create(name: "Earl Blumenauer", political_party: "Democrat", title: "U.S. Representative, Oregon", twitter_handle: "BlumenauerMedia", twitter_profile: "Rep. Earl Blumenauer (OR-03) congressional account. Tweets are by staff." image: "earl_blumenauer.png")
+
+Politician.create(name: "Kevin Brady", political_party: "Republican", title: "U.S. Representative, Texas", twitter_handle: "RepKevinBrady", twitter_profile: "Dad. Husband. Chairman of @WaysandMeansGOP. Deputy Whip. Loves: Baseball and Texas." image: "kevin_brady.png")
+
+Politician.create(name: "Corrine Brown", political_party: "Democrat", title: "U.S. Representative, Florida", twitter_handle: "RepCorrineBrown", twitter_profile: "I have served in the U.S. House of Representatives since 1992. My district includes Jacksonville, Gainesville and Orlando." image: "corrine_brown.png")
+
+Politician.create(name: "Bob Brady", political_party: "Democrat", title: "U.S. Representative, Pennsylvania", twitter_handle: "RepBrady", twitter_profile: "Proudly representing Pennsylvania's 1st District in House of Representatives. Born & Raised in Philadelphia. Fighting for Equality and Democracy." image: "bob_brady.png")
+
+Politician.create(name: "Marsha Blackburn", political_party: "Republican", title: "U.S. Representative, Pennsylvania", twitter_handle: "MarshaBlackburn", twitter_profile: "Proudly representing Pennsylvania's 1st District in House of I am honored to serve the people of Tennessee's 7th District." image: "marsha_blackburn.png")
+
+Politician.create(name: "Michael Burgess", political_party: "Republican", title: "U.S. Representative, Texas", twitter_handle: "MichaelCBurgess", twitter_profile: "Congressman for Texas' 26th District, M.D. (Ob/Gyn) in my former life." image: "michael_burgess.png")
+
+Politician.create(name: "Rob Bishop", political_party: "Republican", title: "U.S. Representative, Utah", twitter_handle: "RepRobBishop", twitter_profile: "" image: "rob_bishop.png")
+
+Politician.create(name: "G.K. Butterfield", political_party: "Democrat", title: "U.S. Representative, North Carolina", twitter_handle: "GKButterfield", twitter_profile: "U. S. Representative, proudly serving North Carolina's 1st Congressional District. Follow my work in Washington and #NC01." image: "gk_butterfield.png")
+
+Politician.create(name: "Charles Boustany", political_party: "Republican", title: "U.S. Representative, Louisiana", twitter_handle: "RepBoustany", twitter_profile: "U. S. Representative, proudly serving North Carolina's 1st Congressional District. Follow my work in Washington and #NC01." image: "charles_boustany.png")
+
+
+Politician.create(name: "Gus Bilirakis", political_party: "Republican", title: "U.S. Representative, Florida", twitter_handle: "RepGusBilirakis", twitter_profile: "Congressman for Florida’s 12th Congressional District, Committees: @HouseCommerce @HouseVetAffairs" image: "gus_bilirakis.png")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
